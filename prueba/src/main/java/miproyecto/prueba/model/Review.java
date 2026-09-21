9a6a09179f02db7c192a46bd57518088d265f4f5
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-09-21 13:23:07 -0500

    feat: add annotations of JPA

--- prueba/src/main/java/miproyecto/prueba/model/Review.java
+++ prueba/src/main/java/miproyecto/prueba/model/Review.java
@@ -1,9 +1,21 @@
 package miproyecto.prueba.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "reviews")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private Long bookId;
+
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String content;
 
     public Review(Long id, Long bookId, String username, String content) {
@@ -11,6 +23,10 @@ public class Review {
         this.bookId = bookId;
         this.username = username;
         this.content = content;
+    }
+
+    public Review() {
+
     }
 
     public Long getId() {
